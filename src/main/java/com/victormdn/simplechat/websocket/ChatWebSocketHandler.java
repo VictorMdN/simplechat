238baee2ac30feae561f98cbfe52789fbcaa78ee
Victor Matheus <victormatheusdonascimento@gmail.com>
2025-02-23 02:02:27 -0300

    add public chat

---
+++ src/main/java/com/victormdn/simplechat/websocket/ChatWebSocketHandler.java
@@ -0,0 +1,38 @@
+package com.victormdn.simplechat.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Slf4j
+@Component
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+    private static final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        log.info(session.getId());
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                webSocketSession.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+    }
+}
